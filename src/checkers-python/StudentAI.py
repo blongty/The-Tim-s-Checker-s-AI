34e3895412f6049ed0049497065d5e5d3b25e5b7
Timothy Lin <timothylin1199@gmail.com>
2019-10-28 13:23:08 -0700

    First draft of MiniMax

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -18,26 +18,9 @@ class StudentAI():
         self.color = 2
         self.depth = 0
     def get_move(self,move):
-        move = minMaxSearch(self.board);
+        return minMaxSearch(self.board)
 
-
-
-    def minMaxSearch(state):
-        return max(
-                minValue(self.board.make_move(oppMove,self.opponent[self.color]) for oppMove in self.board.get_all_possible_moves(self.opponent[self.color]))
-                );
-
-    def minValue(state):
-        self.depth++
-        if(depth >= DEPTH_LIMIT):
-            evalFunction(state)
-        ourMoves = state.get_all_possible_moves(self.color)
-        minVal = 
-        for ourMove in ourMoves:
-            minVal = min(minVal, )
-       
-       
-        # if len(move) != 0:
+         # if len(move) != 0:
         #     self.board.make_move(move,self.opponent[self.color])
         # else:
         #     self.color = 1
@@ -47,3 +30,66 @@ class StudentAI():
         # move = moves[index][inner_index]
         # self.board.make_move(move,self.color)       
         # return move
+
+    def minMaxSearch(self, state):
+        ourMoves = state.get_all_possible_moves(self.color)
+        maxVal = float('-inf')
+        for ourMove in ourMoves:
+            state.make_move(ourMove, self.color)
+            tempMax = minValue(state)
+            if maxVal < tempMax:
+                maxVal = tempMax
+                chosenMove = ourMove
+            state.undo()
+        return chosenMove
+       
+    def maxValue(self, state):
+        self.depth += 1
+        if(depth >= DEPTH_LIMIT):
+            evalFunction(state)
+        ourMoves = state.get_all_possible_moves(self.color)
+        maxVal = float('-inf')
+        for ourMove in ourMoves:
+            state.make_move(ourMove, self.color)
+            maxVal = max(maxVal, self.maxValue(state))
+            state.undo()
+        return maxVal     
+
+    
+    def minValue(self, state):
+        self.depth += 1
+        if(depth >= DEPTH_LIMIT):
+            evalFunction(state)
+        oppMoves = state.get_all_possible_moves(self.opponent[self.color])
+        minVal = float('inf')
+        for oppMove in oppMoves:
+            state.make_move(oppMove, self.opponent[self.color])
+            minVal = min(minVal, self.minValue(state))
+            state.undo()
+        return minVal  
+    
+
+
+    def evalFunction(self, state):
+        ourCount = 0
+        oppCount = 0
+        for row in state.board:
+            for col in row:
+                checkerPiece = state.board[row][col]
+                if checkerPiece.color == "B" and self.color == 1:
+                    if(checkerPiece.is_king):
+                        ourCount += 2
+                    else:
+                        ourCount += 1
+                elif checkerPiece.color == "W" and self.color != 1:
+                    if(checkerPiece.is_king):
+                        oppCount += 2
+                    else:
+                        oppCount += 1
+
+        return ourCount - oppCount
+                    
+
+                    
+                
+
